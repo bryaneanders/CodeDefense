be897f624c5952f4d4fe86a711fe40f4d7aacfa9
sam25 <gosu747@hotmail.com>
2014-04-19 02:17:13 -0700

    Filename secure read
    Check this out, Compile and run it and try to break it..if you can!

--- DefendTheFort.java
+++ DefendTheFort.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 	
-import javax.swing.filechooser.FileNameExtensionFilter;
-	
-public final class InputIsEvil // changed class to final
+public final class DefendTheFort // changed class to final
 {
 	private static Scanner kb = null;		// added global variables
 	private final static int MAX_BUF = 50;
@@ -29,14 +27,14 @@ public final class InputIsEvil // changed class to final
         String name = "";
         Matcher match; 
         boolean isValid = false;
-        	// asking user for only the name of the file without extension and adding ".txt" if input
-        	// is validated
+        	// asking user for only the name of the file without extension and adding ".txt" 
+        	// if input is validated
         while ( !isValid )
         	{
         		System.out.print("Please enter the output file name WITHOUT extension: ");
         		name = kb.next();
         		match = regx.matcher(name);
-        		if ( match.matches() == true && name.length() < 50 )
+        		if ( match.matches() == true && name.length() < MAX_BUF )
         		{
         			name += ".txt";
         			isValid = true;
