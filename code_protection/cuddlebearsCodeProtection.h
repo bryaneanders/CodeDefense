43e265119bfdf313deffefc02522189c66e4b5b0
bryaneanders <bryaneanders@gmail.com>
2014-04-22 08:06:03 -0700

    test from bash shell

--- code_protection/cuddlebearsCodeProtection.h
+++ code_protection/cuddlebearsCodeProtection.h
@@ -4,33 +4,35 @@
 
 #define NAME_LEN 50
 #define FILE_LEN 100
+#define PW_MAX 24
+#define PW_MIN 6
 #define NUM_DUMMIES 5
 #define MAX_ATTEMPTS 5
 #define LINE_LEN 100
 #define DELIMIT " \n.,*()|\\/?><,!@#$%^&*~`;:[]{}abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define DIGITS "0123456789"
-#define HASH_LENGTH 32
+#define HASH_LENGTH 64
 #define SALT_LENGTH 8
 #define INT_LEN 11
 #define EXTENSION ".txt"
 #define EXT_LEN 4
 #define TEMPLATE "_[A-Za-z0-9./]{8}"
 
-/* may need to go to the end of this to write pw's */
-#define DATA_FILE "data.txt"
+/*#define DATA_FILE "data.txt"*/
 
+int safe_ops(int a, int b);
 FILE* open_file(char* fname, const char * mode);
 void get_name(char **name, int lname);
 int valid_name(char *name);
 int get_int();
 int valid_int(char* candidate);
+int check_overflow(char* s);
 void get_fname(char** file, int outfile);
 int min_string_len(char* a, char *b);
 int non_alpha_chars(char* s);
 void append_extension(char **s);
-void init_password(FILE * data);
-int encrypt_password(char **pw, const unsigned char *s, FILE * data) ;
-int convert( uint32_t n );
+void init_password(char **pw);
+int encrypt_password(char **pw, FILE * data) ;
 char* generate_salt();
-int verify_password(FILE* data);
-
+int verify_password(char **pw);
+void flush();
